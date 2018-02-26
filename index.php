8465069ef2ea06ee40d3096b248f4792177726e8
DarioMartinez21 <36760191+DarioMartinez21@users.noreply.github.com>
2018-02-26 10:03:02 +0100

    cambios del viernes

---
+++ index.php
@@ -0,0 +1,48 @@
+!DOCTYPE html>
+
+<html>
+<head>
+    <title>BLOG</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="cabecera">
+    
+</div>
+<div class="cuerpo">
+    
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+?>
